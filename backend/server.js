77f4f72f817b448c558db3761cf4e1484e8cac5e
DeepikaKrishnaa <saideepikarangavajhula@gmail.com>
2026-04-20 23:26:00 +0530

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://mail-scrapper.vercel.app"
+  ],
   credentials: true
 }));
 
